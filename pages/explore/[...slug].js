5137dd208025cc62c2b4ea7e261dd21e3a9649b7
Anushka Palaskar <palaskaranushka6@gmail.com>
2026-05-27 19:53:09 +0530

    Added breadcrumb navigation feature

---
+++ pages/explore/[...slug].js
@@ -0,0 +1,11 @@
+import Breadcrumb from "../../components/Breadcrumb";
+
+export default function ExplorePage() {
+  return (
+    <div>
+      <Breadcrumb />
+
+      <h1>Explore Page</h1>
+    </div>
+  );
+}
